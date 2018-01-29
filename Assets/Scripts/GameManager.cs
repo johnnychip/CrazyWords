e8f01d2528a120dce3772d9983d4f93d3f6a7187
Johnny Chip <johngomez1115@gmail.com>
2018-01-29 16:28:07 -0500

    GameManager added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	
+	public static GameManager Singleton
+	{
+		set; private get;
+	}
+
+	void Awake () 
+	{
+		if(Singleton!=null)
+			DestroyImmediate(gameObject);
+		else
+			Singleton = this;
+	}
+	
+	public void SetImageMode ()
+	{
+
+	}
+
+	public void SetWordsMode ()
+	{
+
+	}
+
+	public void SetAbstractMode ()
+	{
+		
+	}
+
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,16 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class UIManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	[SerializeField]
+	private GameObject imageModeScreen;
+
+	[SerializeField]
+	private GameObject wordsModeScreen;
+
+	[SerializeField]
+	private GameObject abstractModeScreen;
+
+	[SerializeField]
+	private GameObject mainMenuScreen;
+
+	public static UIManager Singleton
+	{
+		get; private set;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Awake ()
+	{
+		if(Singleton!=null)
+			DestroyImmediate(gameObject);
+		else
+			Singleton = this;
 	}
+
+	public void SetActiveImageMode(bool state)
+	{
+		mainMenuScreen.SetActive(false);
+		imageModeScreen.SetActive(true);
+	}
+
+	public void SetActiveWordsMode(bool state)
+	{
+		mainMenuScreen.SetActive(false);
+		wordsModeScreen.SetActive(true);
+	}
+
+	public void SetActiveAbstractMode(bool state)
+	{
+		mainMenuScreen.SetActive(false);
+		abstractModeScreen.SetActive(true);
+	}
+
+	public void SetActiveMainMenuMode(bool state)
+	{
+		imageModeScreen.SetActive(false);
+		wordsModeScreen.SetActive(false);
+		abstractModeScreen.SetActive(false);
+		mainMenuScreen.SetActive(true);
+	}
+
 }
